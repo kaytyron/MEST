7eeb8e92ddedaf4211ba163a6e7f06b68cf65bf6
kaytyron <kelvin.tyron@gmail.com>
2017-09-20 16:16:28 +0000

    School Program

---
+++ Modelling Test.py
@@ -0,0 +1,86 @@
+import random
+
+class School():
+	def __init__(self):
+		self.eit_list = list()
+		self.fellow_list = list()
+	
+	def set_schoolname(self,school):
+		self.school = school
+
+
+	def add_eit(self,Eit):
+		eit_list.append(Eit)
+
+	def add_fellow(self,Fellow):
+		fellow_list.append(Fellow)
+
+	def display_eits(self):
+		print("EIT LIST:")
+		for eit in self.eit_list:
+			eit.get_eit()
+
+	def display_fellows(self):
+		print("FELLOW LIST")
+		for fellow in self.fellow_list:
+			fellow.get_fellow()
+
+	def display_school_mems(self):
+		self.display_fellows()
+		self.display_eits()
+
+
+class Person:
+	def set_name(self,name):
+		self.name = name
+
+	def set_nationality(self,nationality):
+		self.nationality = nationality
+
+
+class Eit(Person):
+	def __init__(self):
+		self.funfact_holder = list()
+		super.__init__()
+
+	def set_name(self,name):
+		self.name = name
+
+	def set_nationality(self,nationality):
+		self.nationality = nationality
+
+	def add_funfact(self,funfact):
+		self.funfact = funfact
+		self.funfact_holder.append(self.funfact)
+
+	def recite_funfact(self):
+		print(random.choice(self.funfact_holder()))
+
+	def get_eit(self):
+		print("EIT name: {0}\n EIT Nationality: {1} \n".format(self.name, self.nationality))
+
+
+
+class Fellow(Person):
+	def __init__(self):
+		self.happiness_code = 0
+		super.__init__()
+
+	def set_name(self,name):
+		self.name = name
+
+	def set_nationality(self,nationality):
+		self.nationality = nationality
+
+	def eat(self, food):
+		happiness_code += 1
+		print("Happiness Level Increased!\n")
+
+	def happiness_level(self):
+		print("Happiness Level = {}\n".format(happiness_code))
+
+	def get_fellow(self):
+		print("Fellow name: {0}\n EIT Nationality: {1}\n".format(self.name, self.nationality))
+
+
+
